bf3c644cfca91ce51012fb096d3b64c0fe829421
Vito <vitocarbone94@gmail.com>
2022-12-07 17:22:17 +0100

    a

--- D4.js
+++ D4.js
@@ -156,38 +156,12 @@ console.log(eyeColor);
   Utilizza uno switch statement per determinare in quale proprietà 
   inserire il personaggio
 */
-for (i = 0; i < starWarsCharacters.length; i++) {
-  switch (true) {
-    case starWarsCharacters[i].eye_color === "blue":
-      eyeColor.blue.push(starWarsCharacters[i]);
-      break;
-    case starWarsCharacters[i].eye_color === "yellow":
-      eyeColor.yellow.push(starWarsCharacters[i]);
-      break;
-    case starWarsCharacters[i].eye_color === "brown":
-      eyeColor.brown.push(starWarsCharacters[i]);
-      break;
-    case starWarsCharacters[i].eye_color === "red":
-      eyeColor.red.push(starWarsCharacters[i]);
-      break;
-    case starWarsCharacters[i].eye_color === "blue-gray":
-      eyeColor["blue-gray"].push(starWarsCharacters[i]);
-      break;
-  }
-}
-console.log(eyeColor);
+
 
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio
 */
-let contatore = 0;
-let massatotale = 0;
-while (contatore < starWarsCharacters.length) {
-  massatotale = massatotale + Number(starWarsCharacters[contatore].mass);
-  // console.log(massatotale);
-  contatore++;
-}
-console.log("massa totale del equipaggio: ", massatotale);
+
 
 /* ESERCIZIO 7
 
@@ -202,42 +176,11 @@ Se la massa è superiore a 1000 stampa in console: "DANGER! OVERLOAD ALERT: Jump
 
 Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
 */
-switch (true) {
-  case massatotale < 500:
-    console.log("Ship is under loaded");
-    break;
-  case massatotale >= 500 && massatotale < 700:
-    console.log("Ship is half loaded");
-    break;
-  case massatotale >= 700 && massatotale < 900:
-    console.log("Warning: Load is over 700");
-    break;
-  case massatotale >= 900 && massatotale < 1000:
-    console.log("Critical Load: Over 900");
-    break;
-  case massatotale >= 1000:
-    console.log("DANGER! OVERLOAD ALERT: Jump ship now!");
-    break;
-}
+
 /* ESERCIZIO 8
 
 Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi creare un nuovo array, o tentare la riassegnazione del valore corrispondente)
 */
-);
-const modified = [];
-for (i = 0; i < starWarsCharacters.length; i++) {
-  if (starWarsCharacters[i].gender === "n/a") {
-    starWarsCharacters[i].gender = "robot";
-    modified.push(starWarsCharacters[i]);
-  }
-
-  // switch (true) {
-  //   case starWarsCharacters[i].gender === "n/a":
-  //     starWarsCharacters[i].gender = "robot";
-  //     break;
-  // }
-}
-console.log(modified); //per non richimare tutti gli oggetti
 
 /* EXTRA ESERCIZIO 9
 
@@ -248,95 +191,7 @@ Usa uno più for loop per raggiungere il risultato
 
 Una volta fatto, crea un console.log() per controllare la proprietà length di "characters" prima e dopo l'operazione
 */
-console.log(characters);
-for (i = 0; i < characters.length; i++) {
-  for (j = 0; j < femaleCharacters.length; j++) {
-    if (characters[i] === femaleCharacters[j].name) {
-      characters.splice(i, 1);
-    }
-  }
-}
-console.log(characters);
+
 /* EXTRA ESERCIZIO 10
 
 Crea una funzionalità che prenda un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo
-*/const random = Math.floor(Math.random() * starWarsCharacters.length);
-if (starWarsCharacters[random].gender === "male") {
-  console.log(
-    "Our randomly choosen character is:",
-    starWarsCharacters[random].name,
-    "--",
-    "His height is:",
-    starWarsCharacters[random].height,
-    "--",
-    "His mass is:",
-    starWarsCharacters[random].mass,
-    "--",
-    "His hair are:",
-    starWarsCharacters[random].hair_color,
-    "--",
-    "His skin is:",
-    starWarsCharacters[random].skin_color,
-    "--",
-    "His eyes are:",
-    starWarsCharacters[random].eye_color,
-    "--",
-    "He was born in the year:",
-    starWarsCharacters[random].birth_year,
-    "--",
-    "and He is a:",
-    starWarsCharacters[random].gender
-  );
-} else if (starWarsCharacters[random].gender === "female") {
-  console.log(
-    "Our randomly choosen character is:",
-    starWarsCharacters[random].name,
-    "--",
-    "Her height is:",
-    starWarsCharacters[random].height,
-    "--",
-    "Her mass is:",
-    starWarsCharacters[random].mass,
-    "--",
-    "Her hair are:",
-    starWarsCharacters[random].hair_color,
-    "--",
-    "Her skin is:",
-    starWarsCharacters[random].skin_color,
-    "--",
-    "Her eyes are:",
-    starWarsCharacters[random].eye_color,
-    "--",
-    "She was born in the year:",
-    starWarsCharacters[random].birth_year,
-    "--",
-    "and She is a:",
-    starWarsCharacters[random].gender
-  );
-} else {
-  console.log(
-    "Our randomly choosen character is:",
-    starWarsCharacters[random].name,
-    "--",
-    "Its height is:",
-    starWarsCharacters[random].height,
-    "--",
-    "Its mass is:",
-    starWarsCharacters[random].mass,
-    "--",
-    "Its hair are:",
-    starWarsCharacters[random].hair_color,
-    "--",
-    "Its skin is:",
-    starWarsCharacters[random].skin_color,
-    "--",
-    "Its eyes are:",
-    starWarsCharacters[random].eye_color,
-    "--",
-    "It was born in the year:",
-    starWarsCharacters[random].birth_year,
-    "--",
-    "and it is a:",
-    starWarsCharacters[random].gender
-  );
-}
